4843490b7e1001429eebe52083239d3fe96ed287
harsa-dev <leofebrianabidanur12@gmail.com>
2026-06-12 14:33:34 +0700

    refactor: export inventory workflow modules

--- artifacts/api-server/src/services/inventory/index.ts
+++ artifacts/api-server/src/services/inventory/index.ts
@@ -6,7 +6,36 @@ export {
   listInventoryItems,
   listStockMovements,
   updateInventoryItem,
-  type InventoryActor,
-  type InventoryDashboardDto,
-  type InventoryItemDto,
 } from "./inventory.service.js";
+
+export {
+  INVENTORY_DASHBOARD_RECENT_MOVEMENT_LIMIT,
+  INVENTORY_TYPES,
+  INVENTORY_UNITS,
+  STOCK_MOVEMENT_REASONS,
+  STOCK_MOVEMENT_TYPES,
+} from "./inventory.constants.js";
+
+export {
+  getInventoryStockStatus,
+  toInventoryDashboardDto,
+  toInventoryItemDto,
+} from "./inventory.dto.js";
+
+export {
+  requireInventoryAdjust,
+  requireInventoryView,
+} from "./inventory.permissions.js";
+
+export type {
+  CreateInventoryItemInput,
+  CreateStockMovementInput,
+  InventoryActor,
+  InventoryDashboardDto,
+  InventoryDashboardSummaryDto,
+  InventoryItemDto,
+  InventoryItemWithCounts,
+  InventoryStockStatus,
+  StockMovementWithItem,
+  UpdateInventoryItemInput,
+} from "./inventory.types.js";
