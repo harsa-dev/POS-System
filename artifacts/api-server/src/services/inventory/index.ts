d3f64b2f0afb6bda5bd2ab8cfef115b75e44c8f8
harsa-dev <leofebrianabidanur12@gmail.com>
2026-06-12 14:14:36 +0700

    refactor: export inventory workflow service

---
+++ artifacts/api-server/src/services/inventory/index.ts
@@ -0,0 +1,12 @@
+export {
+  createInventoryItem,
+  createStockMovement,
+  deleteInventoryItem,
+  getInventoryDashboard,
+  listInventoryItems,
+  listStockMovements,
+  updateInventoryItem,
+  type InventoryActor,
+  type InventoryDashboardDto,
+  type InventoryItemDto,
+} from "./inventory.service.js";
