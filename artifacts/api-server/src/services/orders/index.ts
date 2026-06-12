b1567d1ab2cdaae061250a9aebc5d256168497e2
harsa-dev <leofebrianabidanur12@gmail.com>
2026-06-12 13:09:56 +0700

    refactor: export order status services

--- artifacts/api-server/src/services/orders/index.ts
+++ artifacts/api-server/src/services/orders/index.ts
@@ -1,5 +1,18 @@
 export {
+  checkOrderStatusTransition,
+  legacyOrderStatusPermissions,
+  legacyOrderStatusTransitions,
+  type OrderStatusDecision,
+} from "./order-status-rules.js";
+
+export {
   assertCanMoveOrderStatus,
   canMoveOrderStatus,
   legacyRestaurantOrderTransitions,
 } from "./order-status-workflow.js";
+
+export {
+  transitionOrderStatus,
+  type TransitionOrderStatusInput,
+  type TransitionOrderStatusResult,
+} from "./transition-order-status.service.js";
