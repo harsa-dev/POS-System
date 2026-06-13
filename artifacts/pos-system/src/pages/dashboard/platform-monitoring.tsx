0a9351db697621f544257bfd8204c0328d7cce40
harsa-dev <leofebrianabidanur12@gmail.com>
2026-06-13 17:43:44 +0700

    Add platform monitoring dashboard page

---
+++ artifacts/pos-system/src/pages/dashboard/platform-monitoring.tsx
@@ -0,0 +1,5 @@
+import { DevMonitoringDashboard } from "@/features/shared/platform-monitoring/dev-monitoring-dashboard";
+
+export default function PlatformMonitoringPage() {
+  return <DevMonitoringDashboard />;
+}
