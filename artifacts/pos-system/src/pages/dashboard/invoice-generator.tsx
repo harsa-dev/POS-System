3784fe044d95857e55ebb6f95927bd909267230c
harsa-dev <leofebrianabidanur12@gmail.com>
2026-06-13 19:37:48 +0700

    Wire raw material bridge to invoice generator dashboard

--- artifacts/pos-system/src/pages/dashboard/invoice-generator.tsx
+++ artifacts/pos-system/src/pages/dashboard/invoice-generator.tsx
@@ -1,10 +1,13 @@
 import { InvoiceGeneratorDashboard } from "@/features/shared/invoice-generator";
+import { RawMaterialSharedDashboardBridge } from "@/features/shared/raw-material-bridge";
 import { RetailSharedDashboardBridge } from "@/features/shared/retail-bridge";
 
 export default function InvoiceGeneratorPage() {
   return (
-    <RetailSharedDashboardBridge dashboardId="invoice-generator">
-      <InvoiceGeneratorDashboard />
-    </RetailSharedDashboardBridge>
+    <RawMaterialSharedDashboardBridge dashboardId="invoice-generator">
+      <RetailSharedDashboardBridge dashboardId="invoice-generator">
+        <InvoiceGeneratorDashboard />
+      </RetailSharedDashboardBridge>
+    </RawMaterialSharedDashboardBridge>
   );
 }
