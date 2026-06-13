8ecb70abb27f9a1db8249c35b447145e09dbddbe
harsa-dev <leofebrianabidanur12@gmail.com>
2026-06-14 04:57:48 +0700

    feat(retail): export return API client helper

--- lib/api-client-react/src/index.ts
+++ lib/api-client-react/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./generated/api";
 export * from "./generated/api.schemas";
 export * from "./generated/retail-receiving-status";
+export * from "./generated/retail-returns";
 export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
 export type { AuthTokenGetter } from "./custom-fetch";
