36e9f1d8be532470d9dd0e13f0fc623b7815fde7
harsa-dev <leofebrianabidanur12@gmail.com>
2026-06-13 22:57:05 +0700

    Export raw material storage location services

--- artifacts/api-server/src/services/raw-material/index.ts
+++ artifacts/api-server/src/services/raw-material/index.ts
@@ -1,3 +1,7 @@
+export * from "./raw-material-storage-location.dto.js";
+export * from "./raw-material-storage-location.service.js";
+export * from "./raw-material-storage-location.types.js";
+export * from "./raw-material-storage-location.validation.js";
 export * from "./raw-material-supplier.dto.js";
 export * from "./raw-material-supplier.service.js";
 export * from "./raw-material-supplier.types.js";
