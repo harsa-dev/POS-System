8fec5d794f49d92a2497682dd6d89c906849e972
harsa-dev <leofebrianabidanur12@gmail.com>
2026-06-13 19:18:28 +0700

    feat: add admin role console page

---
+++ artifacts/pos-system/src/pages/dashboard/admin-role-console.tsx
@@ -0,0 +1,5 @@
+import { AdminRoleConsolePage } from "@/features/shared/platform-monitoring/internal-admin-console-page";
+
+export default function AdminRoleConsoleRoute() {
+  return <AdminRoleConsolePage />;
+}
