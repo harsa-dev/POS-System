ff401275ba5df81553b6d784ae5b363d2b22cf35
harsa-dev <leofebrianabidanur12@gmail.com>
2026-06-15 04:48:23 +0700

    refactor(team): export team management components

---
+++ artifacts/pos-system/src/features/shared/team-management/components/index.ts
@@ -0,0 +1,7 @@
+export { AccessChangelogPanel } from "./access-changelog-panel";
+export { AssignmentImportExportPanel } from "./assignment-import-export-panel";
+export { JobRolePresetPanel } from "./job-role-preset-panel";
+export { PermissionMatrixPanel } from "./permission-matrix-panel";
+export { RoleBuilderPanel } from "./role-builder-panel";
+export { RoleRegistryPanel } from "./role-registry-panel";
+export { TeamOverviewCards } from "./team-overview-cards";
